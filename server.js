9a3da9308686d4270dc5c0bb1236f3ae305faa58
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2023-12-28 12:02:38 -0500

    adds routes to apiRoutes and htmlRoutes

--- server.js
+++ server.js
@@ -8,60 +8,15 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // MIDDLEWARES
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Middleware to serve up static assets from the public folder
 app.use(express.static('public'));
 
 // ROUTES
-// POST route for notes page
-app.post('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      let notes = JSON.parse(data);
-      let userInput = req.body;
-      // setting unique ID for each note a user inputs
-      userInput.id = Math.floor(Math.random() * 5000);
-      notes.push(userInput);
-      // writing user input data to db.json file
-    fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
-        res.json(userInput);
-    });
-    }); 
-  });
-  
-  // DELETE route for notes page
-  app.delete('/api/notes/:id', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      let notes = JSON.parse(data);
-      const newInput = notes.filter(note => note.id !== parseInt(req.params.id));
-    
-    // Writing updated data to db.json file
-    fs.writeFile('./db/db.json', JSON.stringify(newInput), (err, data) => {
-      res.json({msg: 'Success'});
-    });
-  });
-  });
-  // GET route for notes by ID
-  app.get('api/notes/:id', (req, res) =>{
-    res.json(notes[req.params.id]);
-  });
-  
-  app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      var notes = JSON.parse(data);
-      res.json(notes);
-    });
-  });
-  
-  app.get('/notes', (req, res) => {
-      res.sendFile(path.join(__dirname, '/notes.html'))
-  });
-  
-  app.get('*', (req, res) => {
-     res.sendFile(path.join(__dirname, '/index.html'));
-  }); 
+require('./routes/apiRoutes.js')(app);
+require('./routes/htmlRoutes.js/index.js')(app);
 
 // START SERVER
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
